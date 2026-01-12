d4ae67c9d48441df3f94f9385631530d9a9c38fb
seanhsueh-prog <aa0983343163@gmail.com>
2026-01-12 23:33:31 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,54 @@
-const CACHE_NAME = 'seafood-boss-v1.6';
-const urlsToCache = [
-  './index.html',
+/* 檔名：service-worker.js (v2.0) */
+const CACHE_NAME = 'seafood-boss-v2.0';
+const FILES_TO_CACHE = [
+  './',
+  './index.html', // 實際上就是 SeafoodBossApp_v2.0.html，請確保伺服器指向正確
   './manifest.json',
   './icons/icon-192.png',
-  './icons/icon-512.png'
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline pages');
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // 對於 CDN 資源採用 Cache First，其他盡量 Network First
+  if (event.request.url.includes('cdn') || event.request.url.includes('unpkg') || event.request.url.includes('cdnjs')) {
+      event.respondWith(
+        caches.match(event.request).then((response) => {
+          return response || fetch(event.request);
+        })
+      );
+  } else {
+      event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+      );
+  }
 });
