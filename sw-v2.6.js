eeb28b5928f3ac87cd565af29025c415bec9edd6
seanhsueh-prog <aa0983343163@gmail.com>
2026-01-14 20:31:10 +0800

    Add files via upload

---
+++ sw-v2.6.js
@@ -0,0 +1,57 @@
+// 檔名：sw-v2.6.js
+// Version 2.6 - AI Resurrection & Cache Force Update
+
+const CACHE_NAME = 'seafood-boss-v2.6';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icons/icon-192.png',
+    'https://cdn.tailwindcss.com',
+    'https://unpkg.com/react@18/umd/react.production.min.js',
+    'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+    'https://unpkg.com/@babel/standalone/babel.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    // API calls to Google Gemini should NEVER be cached
+    if (event.request.url.includes('generativelanguage.googleapis.com')) {
+        return; 
+    }
+
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request).then((fetchRes) => {
+                return caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, fetchRes.clone());
+                    return fetchRes;
+                });
+            });
+        })
+    );
+});
