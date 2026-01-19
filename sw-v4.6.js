ec72aee97fb35443096c3fea711527514ebf73e6
seanhsueh-prog <aa0983343163@gmail.com>
2026-01-19 23:33:12 +0800

    Add files via upload

---
+++ sw-v4.6.js
@@ -0,0 +1,51 @@
+// Service Worker Version 4.6 (Stable & Resilient)
+// 檔名: sw-v4.6.js
+const CACHE_NAME = 'seafood-boss-v4.6-stable';
+const FILES_TO_CACHE = [
+    './',
+    './index.html', // 對應 GitHub Pages
+    './manifest.json',
+    './icons/icon-192.png',
+    './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('[SW] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request)
+            .then((response) => {
+                if (!response || response.status !== 200 || response.type !== 'basic') {
+                    return response;
+                }
+                const responseToCache = response.clone();
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseToCache);
+                });
+                return response;
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
+});
