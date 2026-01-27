b0ac4e7f1659ca94a159549e95f4c2d67f7c0cdd
seanhsueh-prog <aa0983343163@gmail.com>
2026-01-27 22:41:07 +0800

    Add files via upload

---
+++ sw-v7.7.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'seafood-boss-v7.7';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icons/icon-192.png',
+    'https://cdn.tailwindcss.com',
+    'https://unpkg.com/react@18/umd/react.production.min.js',
+    'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+    'https://unpkg.com/@babel/standalone/babel.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
